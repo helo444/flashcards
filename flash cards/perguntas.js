34e14e83e1e31b7e82ad76b9e8cbdba2c82b9c5e
helo444 <132905572+helo444@users.noreply.github.com>
2024-11-05 10:00:04 -0300

    Add files via upload

---
+++ flash cards/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia do Japão',
+    'Qual o nome da montanha mais famosa do Japão?',
+    'Monte Fuji'
+)
+
+criaCartao(
+    'Cultura do Japão',
+    'Qual o nome da vestimenta usada em festivais no Japão?',
+    'Yukata'
+)
+
+criaCartao(
+    'Culinaria do Japão',
+    'Qual a comida mais famosa do Japão?',
+    'O sushi'
+)
+
+criaCartao(
+    'Entretenimento do Japão',
+    'Qual a forma mais comum de entretenimento no Japão?',
+    'Os animes'
+)
